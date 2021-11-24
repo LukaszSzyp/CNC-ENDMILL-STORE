fa422292f5a4499d61c413ffd519319a0fbcc07c
Lukasz Szypulewski <szyp86@gmail.com>
2021-11-24 21:46:31 +0100

    add db services getMillingTolls

--- cnc-endmill/src/component/home/index.js
+++ cnc-endmill/src/component/home/index.js
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { getMillingTolls } from "../../db-data/services";
+
 export const Home = () => {
+  useEffect(() => {
+    getMillingTolls()
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return <h3>Home page</h3>;
 };
