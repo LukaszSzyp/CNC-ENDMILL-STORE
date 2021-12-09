93e6dee304f7916bbd697fba3efc65b84c689e31
Lukasz Szypulewski <szyp86@gmail.com>
2021-12-09 16:09:08 +0100

    add loading product from Data Base

--- cnc-endmill/src/component/products/index.js
+++ cnc-endmill/src/component/products/index.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import { GroupBox } from "../groupBox";
 import * as productsStyled from "./styledProducts";
-import { storeCategoryPL } from "../storeCategory";
+import { getData } from "./../../db-data/services";
 
 export const Products = () => {
+  const [storeCategory, setStoreCategory] = useState([]);
+  const path = "store.json";
+
+  useEffect(() => {
+    getData(path)
+      .then((r) => r.json())
+      .then((data) => {
+        const storeCategory = Object.keys(data).map((item) => ({
+          category: item,
+          subCategory: Object.keys(data[item]).map((subCategoryItem) => ({
+            title: subCategoryItem,
+            imgURL: data[item][subCategoryItem].imgURL,
+          })),
+        }));
+        setStoreCategory(storeCategory);
+      });
+  }, []);
+
   return (
     <productsStyled.Products>
-      {storeCategoryPL.map((category) => (
+      {storeCategory.map((category) => (
         <GroupBox
-          key={category.categoryTitle}
-          title={category.categoryTitle}
+          key={category.category}
+          title={category.category}
           subCategory={category.subCategory}
         />
       ))}
